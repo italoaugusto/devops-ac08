325fb35925c9b24ed0daf9c5dd8f8854555a572d
Wilker Gulyth Torres Lopes <42157961+wilkerlopes05@users.noreply.github.com>
2018-10-22 20:46:29 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -9,4 +9,5 @@ class Curso(models.Model):
     nome = models.CharField(max_length=100)
     periodo = models.CharField(max_length=75)
     instituicao = models.CharField(max_length=200)
-
+class Vestibular(models.Model):
+nome = models.CharField(max_length=200)
